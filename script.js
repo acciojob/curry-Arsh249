8e1831fb9fafbc8fdfd6e3d82a9783ce7c737e31
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-16 15:25:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function curry(callback) {
-  
+    // A function that returns another function to collect the arguments.
+    return function curried(...args) {
+        // If no arguments are passed, invoke the callback with all collected arguments.
+        if (args.length === 0) {
+            return callback();
+        }
+
+        // Return a new function that continues collecting arguments
+        return function(...newArgs) {
+            // If no new arguments are passed, invoke the callback with all accumulated arguments.
+            if (newArgs.length === 0) {
+                return callback(...args);
+            }
+
+            // Otherwise, return the curried function with accumulated arguments.
+            return curried(...args, ...newArgs);
+        };
+    };
 }
 
 module.export=curry
